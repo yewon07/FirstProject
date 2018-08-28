7325b88f2a64cdd6d06d34621c3b0b0086c45148
sohee <sohee@sist54>
2018-08-28 15:24:28 +0900

    first commit

---
+++ 20180828_GitHubProject/src/com/sist/git/MainClass.java
@@ -0,0 +1,10 @@
+package com.sist.git;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Git Hub!!");
+	}
+
+}
