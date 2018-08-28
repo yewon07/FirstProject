729c5325b75bbdb12d317933ea7db61f05878b1e
sohee <sohee@sist54>
2018-08-28 15:30:48 +0900

    1111

--- 20180828_GitHubProject/src/com/sist/git/MainClass.java
+++ 20180828_GitHubProject/src/com/sist/git/MainClass.java
@@ -5,6 +5,10 @@ public class MainClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello Git Hub!!");
+		System.out.println("Hello Git Hub!!");
+		System.out.println("Hello Git Hub!!");
+		System.out.println("Hello Git Hub!!");
+		System.out.println("Hello Git Hub!!");
 	}
 
 }
