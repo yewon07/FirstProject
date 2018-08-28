34c9feb05314b42bcd7e4736f1a40e61a3101a86
sohee <sohee@sist54>
2018-08-28 15:35:20 +0900

    2222

---
+++ 20180828_GitHubProject/src/com/sist/git/MainClass2.java
@@ -0,0 +1,10 @@
+package com.sist.git;
+
+public class MainClass2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("2¹øÂ° git Hub!");
+	}
+
+}
